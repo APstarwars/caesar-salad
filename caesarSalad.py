f2192b6eb22585a919d5a28744eea6bcf567162d
APstarwars <hobbes399@gmail.com>
2021-06-11 12:17:25 -0700

    Extra valid word detection

--- caesarSalad.py
+++ caesarSalad.py
@@ -1,5 +1,6 @@
 import sys
 
+# Takes ciphertext or a file of ciphertext and returns a list of each cracked cipher
 def dcode(ciphertext):
     # Gets ciphers from command line or file
     code = []
@@ -12,10 +13,12 @@ def dcode(ciphertext):
         code.append(ciphertext)
     
     # Cracks each cipher
+    cipherlist = []
     for cipher in code:
+        possibilities = {}
         for shift in range(1, 26):
             output = ""
-            for char in cipher:
+            for char in ciphertext:
                 if char.isalpha():
                     if char.islower():
                         output += chr(((ord(char) + shift - 97) % 26) + 97)
@@ -23,16 +26,26 @@ def dcode(ciphertext):
                         output += chr(((ord(char) + shift - 65) % 26) + 65)
                 elif char != '\n':
                     output += char
-            
+            output = ' ' + output + ' ' # add space padding to help word search
+           
             # Checks for valid english words
             wordFile = open("engmix.txt", "r")
+            wordcount = 0
             for word in wordFile:
-                if (output.lower().find(word.strip('\n')) != -1):
-                    print(output)
-                    break
+                if (output.lower().find(' ' + word.strip('\n') + ' ') != -1):
+                    wordcount += 1
             wordFile.close()
+            if wordcount > 0:
+                possibilities[output.strip()] = wordcount
+        if len(possibilities) > 0:
+            max = list(possibilities.keys())[0]
+            for p in possibilities.keys():
+                if possibilities[p] > possibilities[max]:
+                    max = p
+            cipherlist.append(max)
+    return cipherlist
 
 if len(sys.argv) != 2:
     print("Usage: python caesarSalad.py [ciphertext/file]")
 else:
-    dcode(sys.argv[1])
+    print(dcode(sys.argv[1]))
